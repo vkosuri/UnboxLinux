9c9fb81052be6ba5503ca981eb338a79c398dd9e
vkosuri <malli.kv2@gmail.com>
2022-03-07 17:30:00 +0530

    fix issues

--- README.md
+++ README.md
@@ -7,7 +7,23 @@ This repository main intention is to palce all the instruction at one place to
 ## Ubuntu
 * [Ubuntu 16.04 (xenial)](./ubuntu/ubuntu_16.md)
 * [Ubuntu 20 LTS](./setup.sh)
-wget -O - https://raw.githubusercontent.com/kosuri/UnboxLinux/master/setup.sh | sudo bash
+It waits for password plese input
+wget -O - https://raw.githubusercontent.com/vkosuri/UnboxLinux/master/setup.sh | bash
+
+## Issues
+```
+E: Conflicting values set for option Signed-By regarding source https://apt.kubernetes.io/ kubernetes-xenial: /usr/share/keyrings/kubernetes-archive-keyring.gpg != 
+E: The list of sources could not be read.
+installing curl
+E: Conflicting values set for option Signed-By regarding source https://apt.kubernetes.io/ kubernetes-xenial: /usr/share/keyrings/kubernetes-archive-keyring.gpg != 
+E: The list of sources could not be read.
+E: Conflicting values set for option Signed-By regarding source https://apt.kubernetes.io/ kubernetes-xenial: /usr/share/keyrings/kubernetes-archive-keyring.gpg != 
+E: The list of sources could not be read.
+^C
+```
+Remove the source files
+sudo rm -rf /etc/apt/sources.list.d/kubernetes.list.save
+sudo rm -rf /etc/apt/sources.list.d/kubernetes.list
 
 ## Contributions
 You are most welcome to edit or create content useful to others.
--- setup.sh
+++ setup.sh
@@ -9,41 +9,48 @@ Profile used: ~/.bash_rc
     3. vim
     4. xclip
     5. python3-pip
-    6. vscode
+    6. vscode and extensions
     7. kubernet, minikube, docker
     8. nomachine
     9. postman
     10. vim vimrc create
 ###BLOCK-COMMENT
 
+banner() {
+    msg="# $* #"
+    edge=$(echo "$msg" | sed 's/./#/g')
+    echo "$edge"
+    echo "$msg"
+    echo "$edge"
+}
+
 sudo apt-get update -y
 
-echo 'installing curl' 
+banner "Installing curl, wget, net-tools, snap"
 sudo apt install -y curl wget apt-transport-https net-tools
 
 sleep 5
-echo 'installing git' 
+banner "Installing git"
 sudo apt install git -y
 
 sleep 5
-echo 'installing tool to handle clipboard via CLI'
+banner "Installing tool to handle clipboard via CLI"
 sudo apt-get install xclip -y
 export alias pbcopy='xclip -selection clipboard'
 export alias pbpaste='xclip -selection clipboard -o'
 source ~/.bashrc
 
-echo 'installing vim'
+banner "Installing vim"
 sudo apt install vim -y
-clear
 
 sleep 5
-echo 'installing python-3 pip'
+banner "Installing python-3 pip"
 sudo apt-get install python3-setuptools
 sudo python3 -m easy_install install pip
 python3 -m pip --version
 
 sleep 5
-echo 'installing code'
+banner "Installing vs code"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -52,43 +59,51 @@ sudo apt-get update
 sudo apt-get install code -y # or code-insiders
 
 sleep 5
-echo 'Install kubectl command line utility:'
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-sudo apt-get install software-properties-common -y
-sudo apt-get install curl -y
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+banner "Installing Docker"
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get purge docker-ce docker-ce-cli containerd.io
 sudo apt-get update
-sudo apt-get install -y kubectl
-# Enable tab completion
-echo "source <(kubectl completion bash)" >> ~/.bashrc
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-echo 'Install minikube'
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+banner "Install Docker Engine"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-sleep 5
-echo 'Install Docker 18.06 (Kubernetes does not support newer version of Docker yet)' 
-sudo apt-get remove docker docker-engine docker.io
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update && sudo apt-get install docker-ce=18.06.0~ce~3-0~ubuntu -y
-sudo systemctl start docker
-sudo systemctl enable docker
-
-echo 'Add your user to the user groups so you dont have to "sudo" everytime:'
+# To run Docker without root privileges, see Run the Docker daemon as a non-root user (Rootless mode).
 sudo groupadd docker
-sudo usermod -aG docker $USER 
-sudo groupadd kubectl
-sudo usermod -aG kubectl $USER
-sudo groupadd minikube
-sudo usermod -aG minikube $USER
-chmod 777 /var/run/docker.sock
+sudo usermod -aG docker $USER
+
+echo 'Configure Docker to start on boot'
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 source ~/.bashrc
 docker --version
 docker run hello-world
 
-echo 'getting Minikube started'
-minikube start --driver docker
+
+banner "Install minikube"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo groupadd minikube
+sudo usermod -aG minikube $USER
+minikube start
+
+
+banner "install kubernets"
+sudo snap install kubectl --classic
+kubectl cluster-info
+echo 'Add your user to the user groups so you dont have to "sudo" everytime:'
+sudo groupadd kubectl
+sudo usermod -aG kubectl $USER
 
 echo 'Updating permissions' 
 sudo chown -R $USER $HOME/.kube
@@ -97,21 +112,17 @@ sudo chown -R $USER $HOME/.minikube
 sudo chgrp -R $USER $HOME/.minikube
 
 
-echo 'Check the addons list and enalbe Ingress if you are going to be using ingress controllers'
-minikube addons list
-minikube addons enable ingress
-
 sleep 5
-echo 'installing nomachine'
+banner "installing nomachine"
 wget https://download.nomachine.com/download/7.7/Linux/nomachine_7.7.4_1_amd64.deb
 sudo dpkg -i nomachine_7.7.4_1_amd64.deb
 sudo apt-get install -f
 
-echo 'installing postman'
+banner "installing postman"
 sudo snap install postman
 
-echo 'Updating vmrc file'
-cat <<EOT >> ~/.vimrc 
+banner "Updating vmrc file"
+cat <<EOT >> /home/${USER}/.vimrc 
 "turn on syntax highlighting
 syntax on
 
@@ -138,4 +149,5 @@ set nobackup
 set nowb
 
 EOT
-
+rm -rf nomachine_7.7.4_1_amd64.deb
+rm -rf minikube-linux-amd64
